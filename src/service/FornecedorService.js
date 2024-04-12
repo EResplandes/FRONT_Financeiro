c9971f2e5402199cf588de028ef95cd053134f32
Eduardo C. Resplandes <resplandes930@gmail.com>
2024-04-12 17:15:16 -0300

    Modulo de Financeiro

---
+++ src/service/FornecedorService.js
@@ -0,0 +1,83 @@
+const token = localStorage.getItem('token');
+
+import API_URL from './config.js';
+
+export default class FornecedorService {
+    async cadastraFornecedor(form) {
+        return fetch(`${API_URL}/fornecedores/cadastrar`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                nome: form.nome,
+                nome_fantasia: form.nome_fantasia,
+                cnpj: form.cnpj,
+                login: form.login,
+                senha: form.senha,
+                cod_banco: form.banco,
+                agencia: form.agencia,
+                conta: form.conta,
+                descricao_banco: form.descricao_banco
+            })
+        })
+            .then((res) => res.json())
+            .then((d) => {
+                return d;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                throw error;
+            });
+    }
+
+    async buscaFornecedores() {
+        return await fetch(`${API_URL}/fornecedores/listar`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                Authorization: 'Bearer ' + token
+            }
+        })
+            .then((res) => res.json())
+            .then((d) => {
+                return d;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                throw error;
+            });
+    }
+
+    async editarFornecedor(idFornecedor, form) {
+        return await fetch(`${API_URL}/fornecedores/editar/` + idFornecedor, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                nome: form.nome,
+                nome_fantasia: form.nome_fantasia,
+                cnpj: form.cnpj,
+                login: form.login,
+                senha: form.senha,
+                cod_banco: form.banco,
+                agencia: form.agencia,
+                conta: form.conta,
+                descricao_banco: form.descricao_banco
+            })
+        })
+            .then((res) => res.json())
+            .then((d) => {
+                return d;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                throw error;
+            });
+    }
+}
--- src/views/pages/fornecedor/fornecedor.vue
+++ src/views/pages/fornecedor/fornecedor.vue
@@ -1,50 +1,51 @@
 <script>
 import { ref } from 'vue';
 import { useToast } from 'primevue/usetoast';
-import UnidadeService from '../../../service/UnidadeService.js';
+import FornecedorService from '../../../service/FornecedorService';
 
 export default {
     data() {
         return {
             toast: new useToast(),
             displayConfirmation: ref(false),
-            unidadeService: new UnidadeService(),
+            fornecedorService: new FornecedorService(),
             displayConfirmationActivation: ref(false),
             visibleRight: ref(false),
+            visible2: ref(false),
             filters1: ref(null),
             loading1: ref(null),
-            unidades: ref(null),
-            idUnidade: ref(null),
+            fornecedores: ref(null),
+            idFornecedor: ref(null),
             form: ref({}),
             editar: ref(false),
-            preloading: ref(true)
+            preloading: ref(true),
         };
     },
 
     mounted: function () {
-        // Metódo responsável por buscar todas as unidades
-        this.unidadeService.buscaUnidades().then((data) => {
-            this.unidades = data.response;
+        // Metódo responsável por buscar todas as fornecedores
+        this.fornecedorService.buscaFornecedores().then((data) => {
+            this.fornecedores = data.response;
         });
     },
 
     methods: {
-        // Metódo responsável por buscar todas unidades
-        buscaUnidades() {
-            this.unidadeService.buscaUnidades().then((data) => {
-                this.unidades = data.response;
+        // Metódo responsável por buscar todas fornecedores
+        buscaFornecedores() {
+            this.fornecedorService.buscaFornecedores().then((data) => {
+                this.fornecedores = data.response;
             });
         },
 
-        // Metódo responsável por cadastrar unidade
-        cadastrarUnidade() {
-            this.unidadeService.cadastraUnidade(this.form).then((data) => {
+        // Metódo responsável por cadastrar fornecedor
+        cadastrarFornecedor() {
+            this.fornecedorService.cadastraFornecedor(this.form).then((data) => {
                 console.log(this.form);
                 console.log(data);
 
-                if (data.response == 'Unidade consumidora cadastrada com sucesso!') {
-                    this.showSuccess('Unidade consumidora cadastrada com sucesso!');
-                    this.buscaUnidades();
+                if (data.response == 'Fornecedor cadastrado com sucesso!') {
+                    this.showSuccess('Fornecedor cadastrado com sucesso!');
+                    this.buscaFornecedores();
                     this.form = {};
                 } else {
                     for (const campo in data.errors) {
@@ -60,28 +61,59 @@ export default {
         },
 
         // Metódo responsável por editar unidade
-        editaUnidade() {
-            this.unidadeService.editaUnidade(this.idUnidade, this.form).then((data) => {
-                if (data.response != 'Unidade consumidora atualizada com sucesso!') {
+        editaFornecedor() {
+            this.fornecedorService.editarFornecedor(this.idFornecedor, this.form).then((data) => {
+                console.log(data);
+                if (data.response != 'Fornecedor atualizado com sucesso!') {
                     this.showError('Preencha pelo menos 1 campo.');
                 } else {
-                    this.showSuccess('Unidade consumidora atualizada com sucesso!');
+                    this.showSuccess('Fornecedor atualizado com sucesso!');
                     this.form = {};
-                    this.buscaUnidades();
+                    this.buscaFornecedores();
                 }
             });
         },
 
         btnEditar(id, info) {
-            this.form.unidade = info.nome;
+            this.form.nome = info.nome;
+            this.form.banco = info.cod_banco;
+            this.form.agencia = info.agencia;
+            this.form.conta = info.conta;
+            this.form.descricao_banco = info.descricao_banco;
+            this.form.nome_fantasia = info.nome_fantasia;
+            this.form.cnpj = info.cnpj;
+            this.form.login = info.login;
+            this.form.senha = info.senha;
             this.visibleRight = true;
             this.editar = true;
-            this.idUnidade = id;
+            this.idFornecedor = id;
         },
 
         btnCadastrar() {
             this.visibleRight = true;
             this.editar = false;
+        },
+
+        dadosBancarios(id, info) {
+            this.form.nome = info.nome;
+            this.form.banco = info.cod_banco;
+            this.form.agencia = info.agencia;
+            this.form.conta = info.conta;
+            this.idFornecedor = id;
+        },
+
+        // Metódo responsável por formatar cnpj
+        formatarCNPJ(cnpj) {
+            // Remove caracteres não numéricos
+            cnpj = cnpj.replace(/\D/g, '');
+
+            // Adiciona pontos e traços conforme necessário
+            cnpj = cnpj.replace(/^(\d{2})(\d)/, '$1.$2');
+            cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+            cnpj = cnpj.replace(/\.(\d{3})(\d)/, '.$1/$2');
+            cnpj = cnpj.replace(/(\d{4})(\d)/, '$1-$2');
+
+            return cnpj;
         },
 
         showSuccess(mensagem) {
@@ -105,26 +137,101 @@ export default {
             <ProgressSpinner />
         </div> -->
 
-        <!-- Modal de Cadastro de unidades -->
+        <Dialog v-model:visible="visible2" modal header="Dados Bancários" :style="{ width: '50rem' }"
+            ><br />
+            <div class="grid">
+                <div class="col-12 md:col-12">
+                    <div class="field-checkbox mb-0">
+                        <label class="m-3" for="empresa">Fornecedor: </label>
+                        <InputText disabled v-model="form.nome" />
+                    </div>
+                </div>
+                <div class="col-4 md:col-4">
+                    <div class="field-checkbox mb-0">
+                        <InputText disabled :value="'Banco: ' + form.banco" />
+                    </div>
+                </div>
+                <div class="col-6 md:col-4">
+                    <div class="field-checkbox mb-0">
+                        <InputText disabled :value="'Agência: ' + form.agencia" />
+                    </div>
+                </div>
+                <div class="col-4 md:col-4">
+                    <div class="field-checkbox mb-0">
+                        <InputText disabled :value="'Conta: ' + form.conta" />
+                    </div>
+                </div>
+            </div>
+        </Dialog>
+
+        <!-- Modal de Cadastro de fornecedores -->
         <Sidebar style="width: 500px" v-model:visible="visibleRight" :baseZIndex="1000" position="right">
             <h3 v-if="this.editar == false" class="titleForm">Formulário de Cadastro</h3>
             <h3 v-if="this.editar == true" class="titleForm">Formulário de Edição</h3>
 
             <div class="card p-fluid">
                 <div class="field">
-                    <label for="empresa">Unidade Consumidora: <span v-if="this.editar == false" class="obrigatorio">*</span></label>
-                    <InputText v-model="form.unidade" id="unidade" type="text" required />
+                    <label for="empresa">Nome: <span v-if="this.editar == false" class="obrigatorio">*</span></label>
+                    <InputText v-model="form.nome" id="unidade" type="text" required />
+                </div>
+
+                <div class="field">
+                    <label for="empresa">Nome Fantasia: <span v-if="this.editar == false" class="obrigatorio">*</span></label>
+                    <InputText v-model="form.nome_fantasia" id="nome_fantasia" type="text" required />
+                </div>
+
+                <div class="field">
+                    <label for="empresa">CNPJ: <span v-if="this.editar == false" class="obrigatorio">*</span></label>
+                    <InputText v-model="form.cnpj" id="cnpj" type="text" required maxlength="14" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" />
                 </div>
                 <hr />
+
+                <div class="grid">
+                    <div class="col-12 md:col-6">
+                        <div class="field-checkbox mb-0">
+                            <InputText v-model="form.login" id="login" type="text" required placeholder="Login" />
+                        </div>
+                    </div>
+                    <div class="col-12 md:col-6">
+                        <div class="field-checkbox mb-0">
+                            <InputText v-model="form.senha" id="senha" type="text" required placeholder="Senha" />
+                        </div>
+                    </div>
+                </div>
+
+                <h5 class="titleForm">Dados Bancários <span v-if="this.editar == false" class="obrigatorio">*</span></h5>
+
+                <div class="grid">
+                    <div class="col-12 md:col-4">
+                        <div class="field-checkbox mb-0">
+                            <InputText v-model="form.banco" id="senha" type="text" required placeholder="Banco *" />
+                        </div>
+                    </div>
+                    <div class="col-12 md:col-4">
+                        <div class="field-checkbox mb-0">
+                            <InputText v-model="form.agencia" id="senha" type="text" required placeholder="Agência *" />
+                        </div>
+                    </div>
+                    <div class="col-12 md:col-4">
+                        <div class="field-checkbox mb-0">
+                            <InputText v-model="form.conta" id="senha" type="text" required placeholder="Conta *" />
+                        </div>
+                    </div>
+                    <div class="col-12 md:col-12">
+                        <div class="field-checkbox mb-0">
+                            <InputText v-model="form.descricao_banco" id="senha" type="text" required placeholder="Observação *" />
+                        </div>
+                    </div>
+                </div>
+
                 <div class="field">
-                    <Button v-if="this.editar == false" @click.prevent="cadastrarUnidade()" label="Cadastrar" class="mr-2 mb-2" />
-                    <Button v-if="this.editar == true" @click.prevent="editaUnidade()" label="Editar" class="mr-2 mb-2" />
-                    <Button v-if="this.editar == false" label="importar" class="mr-2 mb-2" />
+                    <Button v-if="this.editar == false" @click.prevent="cadastrarFornecedor()" label="Cadastrar" class="mr-2 mb-2" />
+                    <Button v-if="this.editar == true" @click.prevent="editaFornecedor()" label="Editar" class="mr-2 mb-2" />
                 </div>
             </div>
         </Sidebar>
 
-        <!-- Tabela com todas empresas -->
+        <!-- Tabela com todos fornecedores -->
         <div class="col-12">
             <h2 class="titleForm">Fornecedores</h2>
             <div class="col-12 lg:col-6">
@@ -133,7 +240,7 @@ export default {
             <div class="card">
                 <DataTable
                     dataKey="id"
-                    :value="unidades"
+                    :value="fornecedores"
                     :paginator="true"
                     :rows="10"
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
@@ -158,10 +265,44 @@ export default {
                         </template>
                     </Column>
 
-                    <Column field="Unidade Consumidora" header="Unidade Consumidora" :sortable="true" class="w-2">
+                    <Column field="Nome" header="Nome" :sortable="true" class="w-2">
                         <template #body="slotProps">
-                            <span class="p-column-title">Unidade Consumidora</span>
+                            <span class="p-column-title">Nome</span>
                             {{ slotProps.data.nome }}
+                        </template>
+                    </Column>
+
+                    <Column field="Nome Fantasia" header="Nome Fantasia" :sortable="true" class="w-2">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Nome Fantasia</span>
+                            {{ slotProps.data.nome_fantasia }}
+                        </template>
+                    </Column>
+
+                    <Column field="CNPJ" header="CNPJ" :sortable="true" class="w-2">
+                        <template #body="slotProps">
+                            <span class="p-column-title">CNPJ</span>
+                            {{ formatarCNPJ(slotProps.data.cnpj) }}
+                        </template>
+                    </Column>
+
+                    <Column field="Login" header="Login" :sortable="true" class="w-2">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Login</span>
+                            {{ slotProps.data.login }}
+                        </template>
+                    </Column>
+
+                    <Column field="Senha" header="Senha" :sortable="true" class="w-2">
+                        <template #body="slotProps">
+                            <span class="p-column-title">Senha</span>
+                            {{ slotProps.data.senha }}
+                        </template>
+                    </Column>
+
+                    <Column field="Dados" header="Dados Bancários" :sortable="true" class="w-2">
+                        <template #body="slotProps">
+                            <Button @click.prevent="dadosBancarios(slotProps.data.id, slotProps.data)" icon="pi pi-search" @click="visible2 = true" />
                         </template>
                     </Column>
 
